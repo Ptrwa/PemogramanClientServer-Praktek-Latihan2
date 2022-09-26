23c945b93d9939b05bc2886417ea4e970cef900b
Ptrwa <113502703+Ptrwa@users.noreply.github.com>
2022-09-26 08:07:20 +0700

    Add files via upload

---
+++ latihan2-service/src/test/java/com/putri/latihan2service/Latihan2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putri.latihan2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
